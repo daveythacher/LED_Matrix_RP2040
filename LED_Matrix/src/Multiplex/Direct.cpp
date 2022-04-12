4988e58930f986bc95c264fbb6749741510f9154
David Thacher <davidethacher@gmail.com>
2022-04-12 13:35:41 -0400

    Add files via upload

---
+++ LED_Matrix/src/Multiplex/Direct.cpp
@@ -0,0 +1,8 @@
+#include "Multiplex/Direct.h"
+#include "hardware/gpio.h"
+
+void __not_in_flash_func(Direct::SetRow)(int row) {
+    gpio_clr_mask(0x1F << 11);
+    gpio_set_mask((row % 0x1F) << 11);
+}
+
---
+++ LED_Matrix/src/Multiplex/Multiplex.cpp
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include "Multiplex/Decoder.h"
+#include "Multiplex/Direct.h"
+
+Multiplex *Multiplex::ptr = nullptr;
+
+Multiplex::Multiplex() {
+    // Do nothing
+}
+
+Multiplex::~Multiplex() {
+    // Do nothing
+}
+
+Multiplex *Multiplex::getMultiplexer(int num) {
+    if (ptr != nullptr)
+        return ptr;
+        
+    switch (num) {
+        case 0:
+            ptr = new Decoder();
+            break;
+        case 1:
+            ptr = new Direct();
+            break;
+        default:
+            assert(false);
+    }
+    
+    return ptr;
+}
