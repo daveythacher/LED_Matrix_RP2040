b789e2e3190cd585f9c7c78cfa1588f8164f5bb4
David Thacher <davidethacher@gmail.com>
2022-05-02 20:12:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 git submodule update --init
-cd pico-sdk/
+cd LED_Matrix/lib/pico-sdk/
 git submodule update --init
-cd ..
-DIR=$PWD
-export PICO_SDK_PATH=$PWD/LED_Matrix/lib/pico-sdk
+cd ../FreeRTOS/FreeRTOS-Kernel/
+git submodule update --init
+cd ../../../..
+DIR=$(pwd)
+export PICO_SDK_PATH=$DIR/LED_Matrix/lib/pico-sdk
 cd LED_Matrix
 mkdir build
-cp lib/pico-sdk/external/pico_sdk_import.cmake .
+cp $DIR/LED_Matrix/lib/pico-sdk/external/pico_sdk_import.cmake .
 cd build
 cmake .. -DDEFINE_MULTIPLEX=16 -DDEFINE_MAX_RGB_LED_STEPS=500 -Dapp1=TRUE -Dapp2=TRUE -Dapp3=TRUE
 make -j $(($(nproc) * 2)) #VERBOSE=1
