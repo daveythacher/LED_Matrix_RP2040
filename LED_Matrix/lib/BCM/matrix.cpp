8e2283d35400ddb198a02e46c2a9e321cd56dbad
David Thacher <davidethacher@gmail.com>
2022-05-03 20:21:35 -0400

    Updates

--- LED_Matrix/lib/BCM/matrix.cpp
+++ LED_Matrix/lib/BCM/matrix.cpp
@@ -6,7 +6,6 @@
  
 #include <stdint.h>
 #include "pico/stdlib.h"
-#include "pico/multicore.h"
 #include "hardware/pio.h"
 #include "hardware/gpio.h"
 #include "hardware/dma.h"
--- build.sh
+++ build.sh
@@ -8,16 +8,16 @@ cd ../../../..
 DIR=$(pwd)
 export PICO_SDK_PATH=$DIR/LED_Matrix/lib/pico-sdk
 cd LED_Matrix
-rm -rf build
 mkdir build
+rm build/CMakeCache.txt
 cp $DIR/LED_Matrix/lib/pico-sdk/external/pico_sdk_import.cmake .
 cd build
 #cmake .. -DDEFINE_MULTIPLEX=8 -DDEFINE_MAX_RGB_LED_STEPS=500 -DDEFINE_MAX_REFRESH=200 -Dapp1=TRUE                           #Max resolution of 16x128 200Hz 18-bit color - Runs out of serial bandwidth
-cmake .. -DDEFINE_MULTIPLEX=8 -DDEFINE_MAX_RGB_LED_STEPS=250 -DDEFINE_MAX_REFRESH=200 -Dapp1=TRUE                           #Max resolution of 16x128 400Hz 15-bit color - Near P6 refresh limit
+#cmake .. -DDEFINE_MULTIPLEX=8 -DDEFINE_MAX_RGB_LED_STEPS=250 -DDEFINE_MAX_REFRESH=200 -Dapp1=TRUE                           #Max resolution of 16x128 400Hz 15-bit color - Near P6 refresh limit
 #cmake .. -DDEFINE_MULTIPLEX=8 -DDEFINE_MAX_RGB_LED_STEPS=62 -DDEFINE_MAX_REFRESH=400 -Dapp2=TRUE                            #Max resolution of 16x128 400Hz 9-bit color - Code issue limits color depth
 
 #cmake .. -DDEFINE_MULTIPLEX=16 -DDEFINE_MAX_RGB_LED_STEPS=500 -DDEFINE_MAX_REFRESH=200 -Dapp1=TRUE                          #Max resolution of 32x128 200Hz 15-bit color - Runs out of serial bandwidth
-#cmake .. -DDEFINE_MULTIPLEX=16 -DDEFINE_MAX_RGB_LED_STEPS=500 -Dapp2=TRUE                                                   #Max resolution of 32x128 3.8kHz 15-bit color
+cmake .. -DDEFINE_MULTIPLEX=16 -DDEFINE_MAX_RGB_LED_STEPS=500 -Dapp2=TRUE                                                   #Max resolution of 32x128 3.8kHz 15-bit color
 #cmake .. -DDEFINE_MULTIPLEX=16 -DDEFINE_MAX_RGB_LED_STEPS=500 -Dapp3=TRUE                                                   #Max resolution of 32x256 3.8kHz 15-bit color - Runs out of memory
 
 #cmake .. -DDEFINE_MULTIPLEX=32 -DDEFINE_MAX_RGB_LED_STEPS=500 -DDEFINE_MAX_REFRESH=200 -Dapp1=TRUE                          #Max resolution of 64x64 200Hz 12-bit color - Runs out of serial bandwidth
