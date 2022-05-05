3ca8127ac1394ca2d148cb33b6ac87989dbcf232
David Thacher <davidethacher@gmail.com>
2022-05-04 23:59:02 -0400

    Updates

--- LED_Matrix/lib/SPWM/worker.cpp
+++ LED_Matrix/lib/SPWM/worker.cpp
@@ -99,7 +99,7 @@ static void __not_in_flash_func(set_pixel)(uint8_t x, uint8_t y, uint8_t r0, uin
     uint8_t *c[6] = { index_table[r0][0], index_table[g0][1], index_table[b0][2], index_table[r1][3], index_table[g1][4], index_table[b1][5] };
 
     for (uint32_t i = 0; i < (1 << lower); i++) {
-        uint8_t *p = &buf[bank][y][i * 2][x];
+        uint8_t *p = &buf[bank][y][i][x];
         *p = *c[0] + *c[1] + *c[2] + *c[3] + *c[4] + *c[5];
         for (uint32_t j = 0; j < 6; j++)
             ++c[j];
---
+++ build.groovy
@@ -0,0 +1,70 @@
+#!/usr/bin/env groovy
+
+def cfgs = [
+        [
+            name: "test",
+            enable: true,
+            app: "app1",
+            multiplex: "16",
+            multiplex_num: "1",
+            max_rgb_led_steps: "500",
+            max_refresh: "200",
+            fps: "30",
+            columns: "128",
+            serial_clock: "15.625",
+            blank_time: "1"
+        ],
+        [
+            name: "test2",
+            enable: true,
+            app: "app2",
+            multiplex: "16",
+            multiplex_num: "1",
+            max_rgb_led_steps: "500",
+            max_refresh: "3840",
+            fps: "30",
+            columns: "128",
+            serial_clock: "15.625",
+            blank_time: "1"
+        ]
+    ]
+
+stop = false
+
+def build_flavor(Map c) {
+    if (!c.enable)
+        return "Skipping"
+    new File("./LED_Matrix/build/" + c.name).mkdirs()
+    File f = new File("./LED_Matrix/build/" + c.name + "/build.sh")
+    f.text = ""
+    f << "#!/bin/bash\n"
+    f << "cd ./LED_Matrix/build/" + c.name + "\n"
+    f << "DIR=\$(pwd)\n"
+    f << "export PICO_SDK_PATH=\$DIR/../../lib/pico-sdk\n"
+    f << "cp \$DIR/../../lib/pico-sdk/external/pico_sdk_import.cmake ../..\n"
+    f << "mkdir build\n"
+    f << "cd build\n"
+    f << sprintf("cmake ../../.. -D%s=TRUE -DDEFINE_MULTIPLEX=%s -DDEFINE_MULTIPLEX_NUM=%s -DDEFINE_MAX_RGB_LED_STEPS=%s -DDEFINE_MAX_REFRESH=%s -DDEFINE_FPS=%s -DDEFINE_COLUMNS=%s -DDEFINE_SERIAL_CLOCK=%s -DDEFINE_BLANK_TIME=%s 2>&1\n", c.app, c.multiplex, c.multiplex_num, c.max_rgb_led_steps, c.max_refresh, c.fps, c.columns, c.serial_clock, c.blank_time)
+    f << "make -j \$((\$(nproc) * 2)) 2>&1 #VERBOSE=1\n"
+    f << "if [ \$? -eq 0 ]; then\n"
+    f << "\techo \"Binary output:\"\n"
+    f << "\tls -la \$DIR/build/src/led_*.*\n"
+    f << "else\n"
+    f << "\texit 1\n"
+    f << "fi\n"
+    sprintf("chmod +x ./LED_Matrix/build/%s/build.sh", c.name).execute().waitFor()
+    def proc = sprintf("./LED_Matrix/build/%s/build.sh", c.name).execute()
+    proc.waitFor()
+    if (proc.exitValue() == 1) {
+        stop = true
+    }
+    return proc.text
+}
+
+cfgs.each {
+    print "\n\nBuilding " + it.name + ":\n"
+    print build_flavor(it)
+    if (stop)
+        System.exit(1)
+}
+
