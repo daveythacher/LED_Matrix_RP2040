d155919ebf22195dedd491acbbaacbb4391debe7
David Thacher <davidethacher@gmail.com>
2022-05-13 07:53:52 -0400

    WIP

--- LED_Matrix/src/HT1632C/isr.cpp
+++ LED_Matrix/src/HT1632C/isr.cpp
@@ -7,20 +7,10 @@
 #include "pico/stdlib.h"
 #include "hardware/irq.h"
 
-extern void dma_isr();
-
-void matrix_dma_isr() __attribute__ ((weak));
-void serial_dma_isr() __attribute__ ((weak));
+extern void matrix_dma_isr();
+extern void serial_dma_isr();
 void matrix_pio_isr() __attribute__ ((weak));
 void serial_pio_isr() __attribute__ ((weak));
-
-void __not_in_flash_func(matrix_dma_isr)() {
-    // Do nothing
-}
-
-void __not_in_flash_func(serial_dma_isr)() {
-    // Do nothing
-}
 
 void __not_in_flash_func(matrix_pio_isr)() {
     // Do nothing
@@ -30,19 +20,24 @@ void __not_in_flash_func(serial_pio_isr)() {
     // Do nothing
 }
 
-void __not_in_flash_func(dma_isr)() {
-    matrix_dma_isr();
-    serial_dma_isr();
-}
-
 void __not_in_flash_func(pio_isr)() {
     matrix_pio_isr();
     serial_pio_isr();
 }
 
+void __not_in_flash_func(dma_isr0)() {
+    //matrix_dma_isr();
+}
+
+void __not_in_flash_func(dma_isr1)() {
+    serial_dma_isr();
+}
+
 void isr_start() {
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_isr);
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_isr0);
     irq_set_priority(DMA_IRQ_0, 0);
     irq_set_enabled(DMA_IRQ_0, true);   
+    irq_set_exclusive_handler(DMA_IRQ_1, dma_isr1);
+    irq_set_priority(DMA_IRQ_1, 1);
 }
 
