e508808117242b0ceb859bde3c351cddd7b10037
David Thacher <davidethacher@gmail.com>
2022-07-12 01:36:42 -0400

    Updates

---
+++ LED_Matrix/lib/serial_uart/serial_uart.cpp
@@ -0,0 +1,62 @@
+/* 
+ * File:   serial_uart.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ */
+ 
+#include <stdint.h>
+#include "hardware/gpio.h"
+#include "hardware/dma.h"
+#include "hardware/uart.h"
+#include "serial_uart/serial_uart.h"
+
+static serial_uart_callback func;
+static int dma_chan;
+static dma_channel_config c;
+
+static void serial_uart_reload();
+
+void serial_uart_start(serial_uart_callback callback, int dma) {
+    func = callback;
+    dma_chan = dma;
+    
+    // IO
+    gpio_init(0);
+    gpio_init(1);
+    gpio_set_dir(0, GPIO_OUT);
+    gpio_set_function(0, GPIO_FUNC_UART);
+    gpio_set_function(1, GPIO_FUNC_UART);
+    
+    // UART
+    uart_init(uart0, 4000000);
+    
+    // DMA
+    c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+    channel_config_set_write_increment(&c, true);
+    channel_config_set_read_increment(&c, false);
+    channel_config_set_dreq(&c, DREQ_UART0_RX);
+    dma_channel_set_irq1_enabled(dma_chan, true);
+    
+    serial_uart_reload();
+}
+
+void serial_uart_print(const char *s) {
+    uart_puts(uart0, s);
+}
+
+void __not_in_flash_func(serial_uart_reload)() {
+    uint8_t *buf;
+    uint16_t len;
+        
+    func(&buf, &len);
+    dma_channel_configure(dma_chan, &c, buf, &uart_get_hw(uart0)->dr, len, true);
+}
+
+void __not_in_flash_func(serial_uart_isr)() {
+    if (dma_channel_get_irq1_status(dma_chan)) {
+        serial_uart_reload();
+        dma_hw->ints0 = 1 << dma_chan;
+    }
+}
+
---
+++ LED_Matrix/src/uart/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ */
+ 
+#include "serial_uart/serial_uart.h"
+#include "BCM/config.h"
+
+extern void matrix_start();
+extern void serial_start();
+extern void isr_start();
+extern void serial_task();
+
+int main() {
+    matrix_start();
+    serial_start();
+    isr_start();
+    
+    serial_uart_printf("LED Matrix on RP2040\n");
+    serial_uart_printf("Built on %s at %s\n", __DATE__, __TIME__);
+    serial_uart_printf("Display size %dx%d\n", 2*MULTIPLEX, COLUMNS);
+    serial_uart_printf("Target Refresh %dHz using %d PWM bits\n", MAX_REFRESH, PWM_bits);
+    serial_uart_printf("This version is for standard shift register panels only!\n");
+    serial_uart_printf("Traditional PWM is used and implemented using BCM\n");
+    serial_uart_printf("Blanking time is set to %duS\n", BLANK_TIME);
+    
+    while (1) {
+        serial_task();
+    }
+}
+
