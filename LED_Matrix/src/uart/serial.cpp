4660c2ed1392bd936500dc25053f6280ff70e99f
David Thacher <davidethacher@gmail.com>
2022-07-12 22:56:02 -0400

    Updates

--- LED_Matrix/src/uart/serial.cpp
+++ LED_Matrix/src/uart/serial.cpp
@@ -15,25 +15,7 @@ static test buffers[2];
 static uint8_t buffer = 0;
 static int serial_dma_chan;
 
-void serial_task() {
-
-    for (int x = 0; x < COLUMNS; x++) {
-        for (int y = 0; y < (2 * MULTIPLEX); y++) {
-            if ((x % (2 * MULTIPLEX)) == y) {
-                buffers[buffer][y][x][0] = 0;
-                buffers[buffer][y][x][1] = 0;
-                buffers[buffer][y][x][2] = 0;
-            }
-            else {
-                buffers[buffer][y][x][0] = 0xFF;
-                buffers[buffer][y][x][1] = 0xFF;
-                buffers[buffer][y][x][2] = 0xFF;
-            }
-        }
-    }
-    isReady = true;
-    buffer = (buffer + 1) % 2;
-    
+void serial_task() {    
     if (isReady) {    
         multicore_fifo_push_blocking((uint32_t) &buffers[(buffer + 1) % 2]);
         isReady = false;
