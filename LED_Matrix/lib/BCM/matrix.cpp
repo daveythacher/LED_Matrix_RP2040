815ce5c2ab2eaad509c8692a7a764e150b7ac2d7
David Thacher <davidethacher@gmail.com>
2022-07-21 02:58:23 -0400

    WIP

--- LED_Matrix/lib/BCM/matrix.cpp
+++ LED_Matrix/lib/BCM/matrix.cpp
@@ -20,11 +20,12 @@ volatile uint8_t bank = 0;
 static volatile bool stop = false;
 static int dma_chan[2];
 static Multiplex *m;
-static uint8_t *address_table[1 << PWM_bits];
-static uint8_t null[COLUMNS + 1];
+static struct {uint32_t len; uint8_t *data;} address_table[(1 << PWM_bits) + 1];
+static uint8_t null_table[COLUMNS + 1];
 
 void matrix_dma_isr();
-static void send_line(uint32_t rows, uint8_t buffer);
+static void send_line();
+static void load_line(uint32_t rows, uint8_t buffer);
 
 void matrix_start() {
     // Init Matrix hardware
@@ -33,7 +34,7 @@ void matrix_start() {
         gpio_init(i + 8);
         gpio_set_dir(i + 8, GPIO_OUT);
     }
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 8; i++)
         gpio_set_function(i + 8, GPIO_FUNC_PIO0);
     gpio_init(22);
     gpio_set_dir(22, GPIO_OUT);
@@ -41,8 +42,16 @@ void matrix_start() {
     m = Multiplex::getMultiplexer(MULTIPLEX_NUM);
     
     memset(buf, COLUMNS - 1, sizeof(buf));
-    memset(&null[1], 0, COLUMNS);
-    null[0] = COLUMNS;
+    memset(null_table, 0, COLUMNS + 1);
+    null_table[0] = COLUMNS - 1;
+
+    for (uint32_t i = 0; i < (1 << PWM_bits); i++)
+        address_table[i].len = COLUMNS + 1;
+    
+    address_table[(1 << PWM_bits) - 1].data = null_table;
+    address_table[(1 << PWM_bits) - 1].len = COLUMNS + 1;
+    address_table[1 << PWM_bits].data = NULL;
+    address_table[1 << PWM_bits].len = 0;
     
     // Hack to lower the ISR tick rate, accelerates by 2^PWM_bits (Improves refresh performance)
     //  Automates CLK and LAT signals with DMA and PIO to handle Software PWM of entire row
@@ -52,32 +61,34 @@ void matrix_start() {
     //  OE is not used in this implementation and held to low to enable the display
     //      Last shift will display display.
     /*while (1) {
-        counter2 = (1 << PWM_bits); LAT = 0;    // Start of frame, manually push into FIFO (data stream protocol)
+        counter2 = (1 << PWM_bits) - 1; LAT = 0;    // Start of frame, manually push into FIFO (data stream protocol)
         do {
-            counter = COLUMNS;                  // Start of payload, DMA push into FIFO (data stream protocol)
+            counter = COLUMNS - 1;                  // Start of payload, DMA push into FIFO (data stream protocol)
             do {
-                DAT = DATA; CLK = 0;            // Payload data, DMA push into FIFO (data stream protocol)
-                CLK = 1;                        // Automate CLK pulse
-            } while (--counter > 0); CLK = 0;
-            LAT = 1;                            // Automate LAT pulse at end of payload (bitplane shift)
+                DAT = DATA; CLK = 0;                // Payload data, DMA push into FIFO (data stream protocol)
+                CLK = 1;                            // Automate CLK pulse
+            } while (counter-- > 0); CLK = 0;
+            LAT = 1;                                // Automate LAT pulse at end of payload (bitplane shift)
             LAT = 0;
-        } while (--counter2 > 0);
-        IRQ = 1;                                // Call CPU at end of frame
+        } while (counter2-- > 0);
+        IRQ = 1;                                    // Call CPU at end of frame
     }*/
     
     // PIO
     const uint16_t instructions[] = {
         (uint16_t) (pio_encode_pull(false, true) | pio_encode_sideset(2, 0)),   // PIO SM
         (uint16_t) (pio_encode_mov(pio_x, pio_osr) | pio_encode_sideset(2, 0)),
+        (uint16_t) (pio_encode_out(pio_null, 24) | pio_encode_sideset(2, 0)),
         (uint16_t) (pio_encode_pull(false, true) | pio_encode_sideset(2, 0)),
+        (uint16_t) (pio_encode_out(pio_null, 24) | pio_encode_sideset(2, 0)),
         (uint16_t) (pio_encode_mov(pio_y, pio_osr) | pio_encode_sideset(2, 0)),
         (uint16_t) (pio_encode_out(pio_pins, 6) | pio_encode_sideset(2, 0)),    // PMP Program
-        (uint16_t) (pio_encode_nop() | pio_encode_sideset(2, 1)),
-        (uint16_t) (pio_encode_jmp_y_dec(4) | pio_encode_sideset(2, 0)),
+        (uint16_t) (pio_encode_jmp_y_dec(6) | pio_encode_sideset(2, 1)),
+        (uint16_t) (pio_encode_nop() | pio_encode_sideset(2, 2)),
         (uint16_t) (pio_encode_nop() | pio_encode_sideset(2, 2)),
         (uint16_t) (pio_encode_nop() | pio_encode_sideset(2, 0)),
-        (uint16_t) (pio_encode_jmp_x_dec(2) | pio_encode_sideset(2, 0)),
-        (uint16_t) (pio_encode_irq_set(false, 0) | pio_encode_sideset(2, 0))
+        (uint16_t) (pio_encode_jmp_x_dec(3) | pio_encode_sideset(2, 0)),
+        (uint16_t) (pio_encode_jmp(0) | pio_encode_sideset(2, 0))
     };
     static const struct pio_program pio_programs = {
         .instructions = instructions,
@@ -97,11 +108,10 @@ void matrix_start() {
     pio0->sm[0].clkdiv = ((uint32_t) floor(x) << 16) | ((uint32_t) round((x - floor(x)) * 255.0) << 8);
     pio0->sm[0].pinctrl = (2 << PIO_SM0_PINCTRL_SIDESET_COUNT_LSB) | (6 << PIO_SM0_PINCTRL_OUT_COUNT_LSB) | (14 << PIO_SM0_PINCTRL_SIDESET_BASE_LSB) | 8;
     pio0->sm[0].shiftctrl = (1 << PIO_SM0_SHIFTCTRL_AUTOPULL_LSB) | (6 << 25) | (1 << 19);
-    pio0->sm[0].execctrl = (1 << 17) | (10 << 12);
+    pio0->sm[0].execctrl = (1 << 17) | (12 << 12);
     pio0->sm[0].instr = pio_encode_jmp(0);
     hw_set_bits(&pio0->ctrl, 1 << PIO_CTRL_SM_ENABLE_LSB);
     pio_sm_claim(pio0, 0);
-    pio_set_irq0_source_enabled(pio0, pis_interrupt0, true);
     
     // DMA
     bus_ctrl_hw->priority = (1 << 12) | (1 << 8);
@@ -112,39 +122,46 @@ void matrix_start() {
     channel_config_set_read_increment(&c, true);
     channel_config_set_dreq(&c, DREQ_PIO0_TX0);
     channel_config_set_chain_to(&c, dma_chan[1]);
-    dma_channel_configure(dma_chan[0], &c, &pio0_hw->txf[0], NULL, COLUMNS + 1, false);
+    channel_config_set_irq_quiet(&c, true);
+    dma_channel_configure(dma_chan[0], &c, &pio0_hw->txf[0], NULL, 0, false);
+    dma_channel_set_irq0_enabled(dma_chan[0], true); 
     
     c = dma_channel_get_default_config(dma_chan[1]);
     channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
     channel_config_set_read_increment(&c, true);
-    dma_channel_configure(dma_chan[1], &c, &dma_hw->ch[dma_chan[0]].al3_read_addr_trig, NULL, 1, false);
+    channel_config_set_write_increment(&c, true);
+    channel_config_set_ring(&c, true, 3);                                       // 1 << 3 byte boundary on write ptr
+    dma_channel_configure(dma_chan[1], &c, &dma_hw->ch[dma_chan[0]].al3_transfer_count, &address_table[0], 2, false);
     
-    send_line(0, 1);
+    load_line(0, 1);
+    send_line();
 }
 
-void __not_in_flash_func(send_line)(uint32_t rows, uint8_t buffer) {
+void __not_in_flash_func(send_line)() {
+    dma_hw->ints0 = 1 << dma_chan[0];
+    pio_sm_put(pio0, 0, (1 << PWM_bits) - 1);
+    dma_channel_set_read_addr(dma_chan[1], &address_table[0], true);
+}
+
+void __not_in_flash_func(load_line)(uint32_t rows, uint8_t buffer) {
     for (uint32_t i = 0; i < PWM_bits; i++)
         for (uint32_t k = 0; k < (uint32_t) (1 << i); k++)
-            address_table[(1 << i) + k] = buf[buffer][rows][i];
-    address_table[(1 << PWM_bits) - 1] = null;
-    pio_sm_put(pio0, 0, (1 << PWM_bits) - 1);
-    dma_channel_set_read_addr(dma_chan[1], address_table, true);
+            address_table[(1 << i) + k - 1].data = buf[buffer][rows][i];
 }
 
 void __not_in_flash_func(matrix_dma_isr)() {
     static uint32_t rows = 0;
     
-    if (pio_interrupt_get(pio0, 0)) {
-        uint64_t time = time_us_64();                                           // Start a timer with 1uS delay                                    
-        m->SetRow(rows);
+    if (dma_channel_get_irq0_status(dma_chan[0])) {
+        uint64_t time = time_us_64();                                           // Start a timer with 1uS delay
         
         if (++rows >= MULTIPLEX)
-            rows = 0;
+            rows = 0;                      
+        m->SetRow(rows);
+        load_line(rows, (bank + 1) % 2);
 
         while((time_us_64() - time) < BLANK_TIME);                              // Check if timer has expired
-        
-        send_line(rows, (bank + 1) % 2);                                        // Kick off hardware
-        pio_interrupt_clear(pio0, 0);
+        send_line();                                                            // Kick off hardware
     }
 }
 
