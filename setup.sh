38fa5fe7269ba7ac5fc9bed7b5200d0e7a023a00
David Thacher <davidethacher@gmail.com>
2022-07-30 03:27:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y cmake git gcc-arm-none-eabi gcc g++ build-essential groovy
+sudo apt install -y cmake git gcc-arm-none-eabi gcc g++ build-essential groovy python3
 
 git clone --recursive https://github.com/daveythacher/LED_Matrix_RP2040.git
