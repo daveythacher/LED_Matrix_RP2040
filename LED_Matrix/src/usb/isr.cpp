421133e288f92db7be2ea494f65811203007a5f7
David Thacher <davidethacher@gmail.com>
2023-04-09 06:21:31 -0400

    Updates

--- LED_Matrix/src/usb/isr.cpp
+++ LED_Matrix/src/usb/isr.cpp
@@ -23,6 +23,7 @@ void isr_start() {
     irq_set_enabled(DMA_IRQ_0, true);   
     irq_set_exclusive_handler(DMA_IRQ_1, dma_isr1);
     irq_set_priority(DMA_IRQ_1, 1);
-    irq_set_enabled(DMA_IRQ_1, true);  
+    irq_set_enabled(DMA_IRQ_1, true);
+    irq_set_priority(USBCTRL_IRQ, 2);
 }
 
