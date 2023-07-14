b0e429e24b921bf861256f8ef9b1abe171909c34
David Thacher <davidethacher@gmail.com>
2023-07-14 03:49:36 -0400

    Make main loop a ram function. Currently we use copy to RAM, but just in case I want to use defaul

--- LED_Matrix/src/main.cpp
+++ LED_Matrix/src/main.cpp
@@ -4,17 +4,21 @@
  * License: GPL 3.0
  */
  
+#include "pico/multicore.h"
 #include "Matrix/matrix.h"
 #include "Serial/serial.h"
 #include "ISR/isr.h"
+
+void __not_in_flash_func(loop)() {
+    while (1) {
+        serial_task();
+    }
+}
 
 int main() {
     matrix_start();
     serial_start();
     isr_start();
-    
-    while (1) {
-        serial_task();
-    }
+    loop();
 }
 
