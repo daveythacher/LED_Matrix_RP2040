23d9be2d131686c9e796e00413ec23d5bd2e7740
David Thacher <davidethacher@gmail.com>
2023-08-22 23:45:44 -0400

    Updates

--- LED_Matrix/include/Serial/config.h
+++ LED_Matrix/include/Serial/config.h
@@ -15,15 +15,11 @@
     
     typedef DEFINE_SERIAL_RGB_TYPE test[2 * MULTIPLEX][COLUMNS];
     
-    typedef union {
+    union packet {
         test data;
         uint8_t mem[((sizeof(test) / 64) + 1) * 64];
-    } packet;
+    };
 
-    // Bad practice
-    #define STR(s) s##_range_high
-    #define GET_RANGE_HIGH(TYPE) STR(TYPE)
-
-    const uint32_t range_high = GET_RANGE_HIGH(DEFINE_SERIAL_RGB_TYPE);
+    const uint32_t range_high = DEFINE_SERIAL_RGB_TYPE::range_high;
 #endif
 
--- LED_Matrix/include/Serial/types.h
+++ LED_Matrix/include/Serial/types.h
@@ -9,37 +9,37 @@
 
     #include <stdint.h>
 
-    typedef struct {
+    struct RGB24 {
         uint8_t red;
         uint8_t green;
         uint8_t blue;
-    } RGB24;
 
-    const uint32_t RGB24_range_high = 1 << 8;
+        static constexpr  uint32_t range_high = 1 << 8;
+    };
 
-    typedef struct {
+    struct RGB48 {
         uint16_t red;
         uint16_t green;
         uint16_t blue;
-    } RGB48;
 
-    const uint32_t RGB48_range_high = 1 << 16;
+        static constexpr  uint32_t range_high = 1 << 16;
+    };
 
-    typedef struct {
+    struct RGB_222 {
         uint8_t red : 2;
         uint8_t green : 2;
         uint8_t blue : 2;
-    } RGB_222;
 
-    const uint32_t RGB_222_range_high = 1 << 2;
+        static constexpr  uint32_t range_high = 1 << 2;
+    };
 
-    typedef struct {
+    struct RGB_555 {
         uint16_t red : 5;
         uint16_t green : 5;
         uint16_t blue : 5;
-    } RGB_555;
 
-    const uint32_t RGB_555_range_high = 1 << 5;
+        static constexpr uint32_t range_high = 1 << 5;
+    };
 
 #endif
 
