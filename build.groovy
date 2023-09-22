645ce481d7f2e09303003c172745d09a45f41478
David Thacher <davidethacher@gmail.com>
2023-09-22 12:21:46 -0400

    #22 Removing

--- build.groovy
+++ build.groovy
@@ -40,7 +40,7 @@ def handle_matrix_algorithm(cfg, index) {
     name = cfg.build[index].attributes().name
     c = cfg.build[index].matrix[0].attributes()
 
-    if (c.algorithm == "BCM" || c.algorithm == "PWM" || c.algorithm == "ON_OFF") {
+    if (c.algorithm == "BCM" || c.algorithm == "PWM") {
         flags += sprintf(" -D%s_DEFINE_MULTIPLEX=%s -D%s_DEFINE_MULTIPLEX_NAME=%s -D%s_DEFINE_MAX_RGB_LED_STEPS=%s -D%s_DEFINE_MAX_REFRESH=%s -D%s_DEFINE_COLUMNS=%s -D%s_DEFINE_SERIAL_CLOCK=%s -D%s_DEFINE_BLANK_TIME=%s -D%s_DEFINE_ALGORITHM=%s", name, c.multiplex, name, c.multiplex_name, name, c.max_rgb_led_steps, name, c.max_refresh, name, c.columns, name, c.serial_clock, name, c.blank_time, name, c.algorithm)
     }
     else if (c.algorithm == "TLC5958") {
