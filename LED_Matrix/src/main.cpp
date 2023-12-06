acc8b9d65f5a27339a705bcecea900606fdbd544
David Thacher <davidethacher@gmail.com>
2023-12-06 17:12:23 -0500

    Updates

--- LED_Matrix/src/main.cpp
+++ LED_Matrix/src/main.cpp
@@ -6,21 +6,23 @@
 
 #include "pico/stdio_usb.h"
 #include "pico/multicore.h"
+#include "hardware/watchdog.h"
+#include "hardware/timer.h"
 #include "Matrix/matrix.h"
 #include "Serial/serial.h"
 #include "ISR/isr.h"
 
-// TODO: Add watchdog
-//  We only need to guard Core 0's main loop as we are only doing this for picotool
-
-void __not_in_flash_func(loop)() {
+static void __not_in_flash_func(loop)() {
     while (1) {
         serial_task();
+        watchdog_update();          // We are only interested in protecting core 0
     }
 }
 
 int main() {
     stdio_usb_init();
+    busy_wait_ms(1000);             // Make time (1 second) for picotool before system loads
+    watchdog_enable(100, false);    // Make sure picotool can never be disconnected
     matrix_start();
     serial_start();
     isr_start();
