c0ac82a83e3e15f80c2843743cc544b02b607ed9
David Thacher <davidethacher@gmail.com>
2023-12-14 03:33:18 -0500

    updates

---
+++ LED_Matrix/lib/Multiplex/Shifter/Shifter_CBA/Shifter_CBA.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Shifter_CBA.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ */
+
+#include "Multiplex/Multiplex.h"
+#include "hardware/gpio.h"
+
+namespace Multiplex {
+    void init(int rows) {
+        // TODO:
+    }
+    
+    void __not_in_flash_func(SetRow)(int row) {
+        // TODO:
+    }
+}
