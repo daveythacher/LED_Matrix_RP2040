5562cac95090ba6c00d0a2045bcf97a5d8cd81d7
David Thacher <davidethacher@gmail.com>
2023-12-15 05:33:54 -0500

    Updates

--- LED_Matrix/lib/Matrix/PWM/calculator.cpp
+++ LED_Matrix/lib/Matrix/PWM/calculator.cpp
@@ -9,15 +9,13 @@ namespace Matrix::Calculator {
     constexpr uint8_t columns_per_driver = 16;
     constexpr uint8_t fanout_per_clk = 6;
     constexpr uint8_t max_impedance = 50;               // For longer chains may need custom circuit board which adjusts this
-    constexpr uint8_t max_par_cap_pf = 16;              // For longer chains may need custom circuit board which adjusts this
+    constexpr uint8_t max_par_cap_pf = 18;              // For longer chains may need custom circuit board which adjusts this
     constexpr uint8_t min_harmonics = 5;
-    constexpr uint16_t max_refresh_hz = 3000;
-
 
     // LED constants
-    constexpr uint16_t max_led_impedance = 500;
-    constexpr uint8_t max_led_cap_pf = 15;
-    constexpr uint8_t min_led_harmonics = 5;
+    constexpr uint16_t max_led_impedance = 1650;
+    constexpr uint8_t max_led_cap_pf = 18;
+    constexpr uint8_t min_led_harmonics = 3;
 
     static constexpr float get_refresh_overhead() {
         float refresh_overhead = 1000000.0 / (MULTIPLEX * MAX_REFRESH);
@@ -38,18 +36,16 @@ namespace Matrix::Calculator {
     static constexpr void is_brightness_valid() {
         constexpr float led_rise_us = (max_led_impedance * min_led_harmonics * max_led_cap_pf * MULTIPLEX) / 1000000.0;
         constexpr float period_us = 1000000.0 / (MAX_REFRESH * MULTIPLEX);
-        constexpr float brightness = ((period_us / get_refresh_overhead()) - (led_rise_us * 2)) / period_us;
-        constexpr float accuracy = ((period_us / get_refresh_overhead()) - (led_rise_us * 2)) / (period_us / get_refresh_overhead());
+        constexpr float brightness = ((period_us / get_refresh_overhead()) - led_rise_us) / period_us;
+        constexpr float accuracy = ((period_us / get_refresh_overhead()) - led_rise_us) / (period_us / get_refresh_overhead());
 
-        static_assert(brightness > 0.75, "Brightness less than 75 percent is not recommended");
+        static_assert(brightness > 0.7, "Brightness less than 70 percent is not recommended");
         static_assert(accuracy > 0.95, "Accuracy less than 95 percent is not recommended");
     }
 
     static constexpr void is_blank_time_valid() {
-        constexpr float led_rise_us_low = (max_led_impedance * COLUMNS * min_led_harmonics * max_led_cap_pf) / 1000000.0;
-        constexpr float led_fall_us_high = (1000 * COLUMNS * min_led_harmonics * max_led_cap_pf) / 1000000.0;
+        constexpr float led_fall_us_high = (10000 * COLUMNS * max_led_cap_pf) / 1000000.0;
 
-        static_assert(led_rise_us_low < BLANK_TIME, "Blank time is too low for low side");
         static_assert(led_fall_us_high < BLANK_TIME, "Blank time is too low for high side");
     }
 
