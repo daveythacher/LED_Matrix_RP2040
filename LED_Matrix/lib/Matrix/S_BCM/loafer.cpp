c10d85530a95cf99df39940c23e3aa5020c80f93
David Thacher <davidethacher@gmail.com>
2024-01-11 15:14:21 -0500

    Updates

--- LED_Matrix/lib/Matrix/S_BCM/loafer.cpp
+++ LED_Matrix/lib/Matrix/S_BCM/loafer.cpp
@@ -4,15 +4,11 @@
  * License: GPL 3.0
  */
  
-#include <algorithm>
 #include <stdint.h>
-#include <string.h>
-#include <math.h>
 #include "pico/multicore.h"
 #include "Serial/config.h"
 #include "Matrix/matrix.h"
 #include "Matrix/S_BCM/memory_format.h"
-#include "Matrix/helper.h"
 
 namespace Matrix {
     extern test2 buf[];
@@ -24,7 +20,7 @@ namespace Matrix::Worker {
 
 namespace Matrix::Loafer {
     static uint8_t bank = 1;
-    
+
     void __not_in_flash_func(toss)(void *arg) {
         // TODO: 
 
