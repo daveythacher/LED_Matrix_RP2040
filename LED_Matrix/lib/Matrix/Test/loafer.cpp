124e1016184f8191d08d1539f5f9857492b63d6e
David Thacher <davidethacher@gmail.com>
2024-01-12 14:17:35 -0500

    Updates

--- LED_Matrix/lib/Matrix/Test/loafer.cpp
+++ LED_Matrix/lib/Matrix/Test/loafer.cpp
@@ -10,24 +10,9 @@
 #include "Serial/config.h"
 #include "Matrix/matrix.h"
 
-namespace Matrix {
-    extern test2 buf[];
-}
-
-namespace Matrix::Worker {
-    extern volatile bool vsync;
-}
-
 namespace Matrix::Loafer {
-    static uint8_t bank = 1;    // TODO: I think we may want to share this with Matrix::Worker and Matrix now! (make volatile!!!)
-
     void __not_in_flash_func(toss)(void *arg) {
         uart_write_blocking(uart1, (uint8_t *) arg, payload_size);
-
-        if (!Worker::vsync) {
-            bank = (bank + 1) % Serial::num_framebuffers;
-            Worker::vsync = true;
-        }
     }
 
     void *__not_in_flash_func(get_back_buffer)() {
