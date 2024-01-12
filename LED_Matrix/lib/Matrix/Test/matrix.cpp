431abbb2c56f9d7de59cc7659e8f37a862323105
David Thacher <davidethacher@gmail.com>
2024-01-12 14:13:55 -0500

    Updates

--- LED_Matrix/lib/Matrix/Test/matrix.cpp
+++ LED_Matrix/lib/Matrix/Test/matrix.cpp
@@ -6,12 +6,24 @@
  
 #include "pico/multicore.h"
 #include "Matrix/matrix.h"
+#include "hardware/uart.h"
+#include "hardware/gpio.h"
 
 namespace Matrix {
     volatile int timer;
 
     void start() {
         // Do nothing
+
+        const int TX = 8;
+        const int RX = 9;
+
+        gpio_init(TX);
+        gpio_init(RX);
+        gpio_set_dir(TX, GPIO_OUT);
+        gpio_set_function(TX, GPIO_FUNC_UART);
+        gpio_set_function(RX, GPIO_FUNC_UART);
+        uart_init(uart1, 115200);
 
         timer = hardware_alarm_claim_unused(true);
     }
--- LED_Matrix/lib/Matrix/Test/worker.cpp
+++ LED_Matrix/lib/Matrix/Test/worker.cpp
@@ -6,24 +6,12 @@
  
 #include <stdint.h>
 #include "pico/multicore.h"
-#include "hardware/uart.h"
-#include "hardware/gpio.h"
 #include "Serial/config.h"
 #include "Matrix/matrix.h"
 #include "Matrix/helper.h"
 
 namespace Matrix::Worker {
-    void __not_in_flash_func(work)() {
-        const int TX = 8;
-        const int RX = 9;
-
-        gpio_init(TX);
-        gpio_init(RX);
-        gpio_set_dir(TX, GPIO_OUT);
-        gpio_set_function(TX, GPIO_FUNC_UART);
-        gpio_set_function(RX, GPIO_FUNC_UART);
-        uart_init(uart1, 115200);
-        
+    void __not_in_flash_func(work)() {        
         while(1) {
             Serial::packet *p = (Serial::packet *) APP::multicore_fifo_pop_blocking_inline();
 
