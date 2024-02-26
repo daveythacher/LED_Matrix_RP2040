e4309cd77a07e9d28a296ace04d24f75d3a8f8b8
David Thacher <davidethacher@gmail.com>
2024-02-25 19:42:17 -0500

    Reducing memory usage of BCM algorithm. (Experimental)

--- LED_Matrix/lib/Matrix/HUB75/BCM/worker.cpp
+++ LED_Matrix/lib/Matrix/HUB75/BCM/worker.cpp
@@ -23,39 +23,37 @@ namespace Matrix::Worker {
     volatile bool vsync = false;
 
     union index_table_t {
-        uint8_t table[1 << PWM_bits][6][PWM_bits];
-        uint32_t v[(1 << PWM_bits) * 6 * PWM_bits / 4];
+        uint8_t table[16][6][4];
+        uint32_t v[16 * 6];
     };
-
+    
     static index_table_t index_table;
 
-    static uint8_t *__not_in_flash_func(get_table)(uint16_t v, uint8_t i) {
+    template <typename T> static uint8_t *__not_in_flash_func(get_table)(uint16_t v, uint8_t i, uint8_t nibble) {
         constexpr uint32_t div = std::max((uint32_t) Serial::range_high / 1 << PWM_bits, (uint32_t) 1);
         constexpr uint32_t mul = std::max((uint32_t) 1 << PWM_bits / Serial::range_high, (uint32_t) 1);
         
         v = v * mul / div;
         //v %= (1 << PWM_bits);
-        return index_table.table[v][i];
+        return index_table.table[(v >> (nibble * sizeof(T))) & ((1 << sizeof(T)) - 1)][i];
     }
 
     // Forgive the shameless and reckless casting.
     template <typename T> static void __not_in_flash_func(set_pixel)(uint8_t x, uint8_t y, uint16_t r0, uint16_t g0, uint16_t b0, uint16_t r1, uint16_t g1, uint16_t b1) {    
-        T *c[6] = { (T *) get_table(r0, 0), (T *) get_table(g0, 1), (T *) get_table(b0, 2), (T *) get_table(r1, 3), (T *) get_table(g1, 4), (T *) get_table(b1, 5) };
-    
-        for (uint32_t i = 0; i < PWM_bits; i += sizeof(T)) {
+        for (uint32_t nib = 0; nib < PWM_bits; nib += sizeof(T)) {
+            T *c[6] = { (T *) get_table<T>(r0, 0, nib), (T *) get_table<T>(g0, 1, nib), (T *) get_table<T>(b0, 2, nib),
+                    (T *) get_table<T>(r1, 3, nib), (T *) get_table<T>(g1, 4, nib), (T *) get_table<T>(b1, 5, nib) };
+        
             T p = *c[0] + *c[1] + *c[2] + *c[3] + *c[4] + *c[5];
 
-            for (uint32_t j = 0; (j < sizeof(T)) && ((i + j) < PWM_bits); j++)
-                Matrix::buf[bank][y][i + j][x + 1] = (p >> (j * 8)) & 0xFF;
-
-            for (uint32_t j = 0; j < 6; j++)
-                ++c[j];
+            for (uint32_t j = 0; j < sizeof(T); j++)
+                Matrix::buf[bank][y][nib * sizeof(T)][x + 1] = (p >> (j * 8)) & 0xFF;
         }
     }
 
     static void build_index_table() {
-        for (uint32_t i = 0; i < (1 << PWM_bits); i++)
-            for (uint32_t j = 0; j < PWM_bits; j++)
+        for (uint32_t i = 0; i < 16; i++)
+            for (uint32_t j = 0; j < 4; j++)
                 for (uint8_t k = 0; k < 6; k++)
                     if (i & (1 << j))
                         index_table.table[i][k][j] = 1 << k;
