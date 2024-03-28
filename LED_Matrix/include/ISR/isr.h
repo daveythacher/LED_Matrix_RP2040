217b5e0bd935530ecd4e5402e5c2c5108f8fe090
David Thacher <davidethacher@gmail.com>
2024-03-28 10:52:31 -0400

    updates

--- LED_Matrix/include/ISR/isr.h
+++ LED_Matrix/include/ISR/isr.h
@@ -12,6 +12,7 @@ namespace APP {
     /**
      *  @brief Starts ISR handlers on Core 1
      *  @details Implemented in src/<app>/isr.cpp
+     *  @details Must always call after Matrix::start(), to initialize Matrix::timer variable.
      */
     void isr_start_core1();
 }
