d2d0b765da7221349f81159f0a32d0928f75fcad
David Thacher <davidethacher@gmail.com>
2024-06-28 21:16:03 -0400

    updates

--- LED_Matrix/lib/src/Multiplex/Shifter/Shifter.cpp
+++ LED_Matrix/lib/src/Multiplex/Shifter/Shifter.cpp
@@ -1,18 +1,29 @@
 /* 
- * File:   Shifter_ABC.cpp
+ * File:   Shifter.cpp
  * Author: David Thacher
  * License: GPL 3.0
  */
 
 #include "Multiplex/Multiplex.h"
+#include "Multiplex/config.h"
 #include "hardware/gpio.h"
 
 namespace Multiplex {
     void init(int rows) {
         // TODO:
+        switch (multiplex_type) {
+
+            default:
+                break;
+        }
     }
     
     void __not_in_flash_func(SetRow)(int row) {
         // TODO:
+        switch (multiplex_type) {
+
+            default:
+                break;
+        }
     }
 }
