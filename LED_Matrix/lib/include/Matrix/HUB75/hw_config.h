ee5325a1b7cce4f7dabb108bb08f6a1ae4a2f720
David Thacher <davidethacher@gmail.com>
2024-07-04 19:53:13 -0400

    updates

--- LED_Matrix/lib/include/Matrix/HUB75/hw_config.h
+++ LED_Matrix/lib/include/Matrix/HUB75/hw_config.h
@@ -10,11 +10,11 @@
 #include <stdint.h>
 
 namespace Matrix::HUB75 {
-    // R0 G0 B0 R1 G1 B1 CLK LAT
+    // Order (LSB to MSB): R0 G0 B0 R1 G1 B1 CLK LAT
     constexpr uint16_t HUB75_DATA_BASE = 8;
     constexpr uint16_t HUB75_DATA_LEN = 8;
 
-    // A B C D E
+    // Order (LSB to MSB): A B C D E
     constexpr uint16_t HUB75_ADDR_BASE = 16;
     constexpr uint16_t HUB75_ADDR_LEN = 5;
 
