961f12d8df2963407661d6be299831cbb55e9f9c
David Thacher <davidethacher@gmail.com>
2024-08-20 17:32:19 -0400

    Split SIMD implementations

---
+++ LED_Matrix/lib/src/SIMD/Portable/SIMD_QUARTER.cpp
@@ -0,0 +1,27 @@
+#include "SIMD/SIMD.h"
+
+namespace SIMD {
+    template <typename T> const SIMD_QUARTER<T> SIMD_QUARTER<T>::operator|(SIMD_QUARTER<T> const& arg) const {
+        SIMD_QUARTER<T> result;
+        result.l = this->l | arg.l;
+        return result;
+    }
+
+    template <typename T> const SIMD_QUARTER<T> SIMD_QUARTER<T>::operator&(SIMD_QUARTER<T> const& arg) const {
+        SIMD_QUARTER<T> result;
+        result.l = this->l & arg.l;
+        return result;
+    }
+
+    template <typename T> const bool SIMD_QUARTER<T>::operator==(SIMD_QUARTER<T> const& arg) const {
+        bool result = true;
+        
+        for (uint32_t i = 0; i < SIMD_QUARTER<T>::size(); i++) {
+            result &= this->v[i] == arg.v[i];
+        }
+
+        return result;
+    }
+
+    template class SIMD_QUARTER<uint8_t>;
+}
--- LED_Matrix/lib/src/SIMD/Portable/SIMD_SINGLE.cpp
+++ LED_Matrix/lib/src/SIMD/Portable/SIMD_SINGLE.cpp
@@ -1,29 +1,6 @@
 #include "SIMD/SIMD.h"
 
-// TODO: Move these into their own file
 namespace SIMD {
-    template <typename T> const SIMD_QUARTER<T> SIMD_QUARTER<T>::operator|(SIMD_QUARTER<T> const& arg) const {
-        SIMD_QUARTER<T> result;
-        result.l = this->l | arg.l;
-        return result;
-    }
-
-    template <typename T> const SIMD_QUARTER<T> SIMD_QUARTER<T>::operator&(SIMD_QUARTER<T> const& arg) const {
-        SIMD_QUARTER<T> result;
-        result.l = this->l & arg.l;
-        return result;
-    }
-
-    template <typename T> const bool SIMD_QUARTER<T>::operator==(SIMD_QUARTER<T> const& arg) const {
-        bool result = true;
-        
-        for (uint32_t i = 0; i < SIMD_QUARTER<T>::size(); i++) {
-            result &= this->v[i] == arg.v[i];
-        }
-
-        return result;
-    }
-
     template <typename T> const SIMD_SINGLE<T> SIMD_SINGLE<T>::operator|(SIMD_SINGLE<T> const& arg) const {
         SIMD_SINGLE<T> result;
 
@@ -54,6 +31,5 @@ namespace SIMD {
         return result;
     }
 
-    template class SIMD_QUARTER<uint8_t>;
     template class SIMD_SINGLE<uint32_t>;
 }
