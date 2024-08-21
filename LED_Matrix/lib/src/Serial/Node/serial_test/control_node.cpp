ea85dea21e24c4770ac3d4db3d51300f195d9e1a
David Thacher <davidethacher@gmail.com>
2024-08-20 20:01:31 -0400

    Refactoring Serial Node and Protocol

---
+++ LED_Matrix/lib/src/Serial/Node/serial_test/control_node.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   serial.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ */
+ 
+#include <stdint.h>
+#include "pico/multicore.h"
+#include "Serial/Node/Control/serial.h"
+
+namespace Serial::Node::Control {
+    void __not_in_flash_func(task)() {
+        // Do nothing
+    }
+
+    void start() {
+        // Do nothing
+    }
+}
