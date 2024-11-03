59c297866515644a844a1353aa3ef32d64bb2bd5
David Thacher <davidethacher@gmail.com>
2024-11-03 11:21:47 -0500

    Trying to clarify a comment

--- LED_Matrix/lib/src/Serial/Protocol/Serial/serial.cpp
+++ LED_Matrix/lib/src/Serial/Protocol/Serial/serial.cpp
@@ -41,6 +41,11 @@ namespace Serial::Protocol {
         //                  Just becareful of being too tight!!!
         //          I think this ends up being three threads on a dual core. (CPU plus DMA)
         //              I have to manage that ISR manually or clock it to the speed of light.
+        //              Just to attempt to clarify this: (From a memory point of view)
+        //                  Thread 0: IO serial protocol pollers (These run off core 0 and can have some DMA offload)
+        //                  Thread 1: Compute thread (This can be low priority on core 0)
+        //                  Thread 2: Multiplexing thread (This pretty much has to run on core 1 at high priority)
+        //                  Thread 3: DMA IO operations
         Serial::Protocol::CONTROL_NODE::control_node();
         status = Serial::Protocol::DATA_NODE::Command::data_node();
 
