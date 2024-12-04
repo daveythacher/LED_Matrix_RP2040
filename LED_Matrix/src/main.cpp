0dd93b1f1cb8f2c4f65ff488fc47d7766206487c
David Thacher <davidethacher@gmail.com>
2024-12-04 16:46:43 -0500

    Messing around

--- LED_Matrix/src/main.cpp
+++ LED_Matrix/src/main.cpp
@@ -11,8 +11,15 @@
 #include "Serial/Node/Data/serial.h"
 #include "Serial/Protocol/serial.h"
 #include "ISR/isr.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-static void __not_in_flash_func(loop_core0)() {
+static StaticTask_t xLOOP_CORE0Buffer;
+static StaticTask_t xLOOP_CORE1Buffer;
+static StackType_t xLOOP_CORE0_STACK[configMINIMAL_STACK_SIZE];
+static StackType_t xLOOP_CORE1_STACK[configMINIMAL_STACK_SIZE];
+
+static void __not_in_flash_func(loop_core0)(void *args) {
     while (1) {
         // Uses blocking pushes to FIFO.
         Serial::Node::Control::task();
@@ -23,7 +30,7 @@ static void __not_in_flash_func(loop_core0)() {
     }
 }
 
-static void __not_in_flash_func(loop_core1)() {
+static void __not_in_flash_func(loop_core1)(void *args) {
     Matrix::start();                // Note ISR logic should be detected by watchdog. (FIFO will fill up and Matrix::Worker::work will stall.)
     APP::isr_start_core1();         // Matrix ISRs are allocated to this core. (These will slow the FIFO consumption rate.)
 
@@ -38,6 +45,23 @@ int main() {
     Serial::Node::Control::start();
     Serial::Node::Data::start();
     Serial::Protocol::start();
-    multicore_launch_core1(loop_core1);
-    loop_core0();
+    //multicore_launch_core1(loop_core1);
+    //loop_core0();
+
+    xTaskCreateStatic(loop_core0, "core0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, xLOOP_CORE0_STACK, &xLOOP_CORE0Buffer);
+    xTaskCreateStatic(loop_core1, "core1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, xLOOP_CORE1_STACK, &xLOOP_CORE1Buffer);
+	xPortStartScheduler();
+}
+
+// TODO: Sort this out
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName ) {
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
 }
