b1594e0cc4c32665a4133ad3a7d32812f0cda21d
David Thacher <davidethacher@gmail.com>
2024-12-09 11:20:17 -0500

    updates

--- LED_Matrix/lib/src/Concurrent/Queue/Queue.cpp
+++ LED_Matrix/lib/src/Concurrent/Queue/Queue.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Concurrent/Queue/Queue.h"
+#include "Serial/config.h"
 #include "FreeRTOS/FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "queue.h"
@@ -15,12 +16,11 @@ namespace Concurrent {
     template <typename T> Queue<T>::Queue(uint8_t depth) {
         uint8_t *stack = new uint8_t[depth * sizeof(T)];
         StaticQueue_t *queue = new StaticQueue_t;
-
-        handler = xQueueCreateStatic(depth, sizeof(T), area, queue)
+        handle = xQueueCreateStatic(depth, sizeof(T), area, queue)
     }
 
     template <typename T> void Queue<T>::push(T *val) {
-        while (result = xQueueSend(handle, val, 1) != pdTRUE);
+        while (xQueueSend(handle, val, 1) != pdTRUE);
     }
 
     template <typename T> T Queue<T>::pop() {
@@ -28,4 +28,6 @@ namespace Concurrent {
         while (xQueueSend(handle, &val, 1) != pdTRUE); 
         return val;
     }
+
+    template class Queue<Serial::packet>;
 }
