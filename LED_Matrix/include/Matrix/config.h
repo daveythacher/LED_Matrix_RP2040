267901089fff94f59894941b3434966d86b9a2d1
David Thacher <davidethacher@gmail.com>
2025-05-29 02:43:41 -0400

    Adding blob for SPWM frame, work in progress.

--- LED_Matrix/include/Matrix/config.h
+++ LED_Matrix/include/Matrix/config.h
@@ -17,9 +17,13 @@
 #define MULTIPLEXER Decoder     // Multiplex
 
 namespace Matrix {
+    // Only used by BUS8::SPWM
+    constexpr uint16_t STEPS_MAJOR = 16;
+    constexpr uint16_t STEPS_MINOR = 64;
+
     // These are not to exceed 4096 when multiplied together.
     constexpr uint8_t MULTIPLEX = 8;
-    constexpr uint16_t STEPS = 64;
+    constexpr uint16_t STEPS = 64;                                  // For BUS8::SPWM: STEPS_MAJOR + STEPS_MINOR + 1
     constexpr uint8_t COLUMNS = 8;
 
     constexpr uint8_t BLANK_TIME = 10;
---
+++ LED_Matrix/src/Matrix/BUS8/SPWM/Multiplex.cpp
@@ -0,0 +1,231 @@
+/* 
+ * File:   Multiplex.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "pico/platform.h"
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+#include "hardware/dma.h"
+#include "hardware/sync.h"
+#include "pico/multicore.h"
+#include "hardware/structs/bus_ctrl.h"
+#include "Matrix/BUS8/SPWM/Multiplex.h"
+#include "Multiplex/Multiplex.h"
+#include "Matrix/BUS8/SPWM/Programs.h"
+#include "Matrix/BUS8/hw_config.h"
+#include "GPIO/GPIO.h"
+
+namespace Matrix::BUS8::SPWM {
+    // PIO Protocol
+    //  There are 2^PWM_bits shifts per period.
+    //      The serial protocol used by PIO is column length decremented by one followed by column values.
+    //          The PIO logic is indexed from zero, and there is no way to command zero transfer length.
+    //      The serial protocol has a header which indicates the number of transfers to expect
+    //          This is loaded by the CPU manually before starting DMA (excludes null termination transfer)
+    //  There are 2^PWM_bits plus two transfers.
+    //      The second to last transfer turns the columns off before multiplexing. (Standard shift)
+    //      The last transfer stops the DMA and fires an interrupt
+    Multiplex::Multiplex() {
+        // Init Matrix hardware
+        // IO
+        for (int i = 0; i < ::Matrix::BUS8::BUS8_DATA_LEN; i++) {
+            gpio_init(i + ::Matrix::BUS8::BUS8_DATA_BASE);
+            gpio_set_dir(i + ::Matrix::BUS8::BUS8_DATA_BASE, GPIO_OUT);
+            gpio_set_function(i + ::Matrix::BUS8::BUS8_DATA_BASE, GPIO_FUNC_PIO0);
+            IO::GPIO::claim(i + ::Matrix::BUS8::BUS8_DATA_BASE);
+        }
+
+        gpio_init(::Matrix::BUS8::BUS8_OE);
+        gpio_init(::Matrix::BUS8::BUS8_RCLK);
+        gpio_init(::Matrix::BUS8::BUS8_FCLK);
+        gpio_set_dir(::Matrix::BUS8::BUS8_OE, GPIO_OUT);
+        gpio_set_dir(::Matrix::BUS8::BUS8_RCLK, GPIO_IN);
+        gpio_set_dir(::Matrix::BUS8::BUS8_FCLK, GPIO_IN);
+        gpio_set_function(::Matrix::BUS8::BUS8_OE, GPIO_FUNC_SIO);
+        gpio_set_function(::Matrix::BUS8::BUS8_RCLK, GPIO_FUNC_SIO);
+        gpio_set_function(::Matrix::BUS8::BUS8_FCLK, GPIO_FUNC_SIO);
+        IO::GPIO::claim(::Matrix::BUS8::BUS8_OE);
+        IO::GPIO::claim(::Matrix::BUS8::BUS8_RCLK);
+        IO::GPIO::claim(::Matrix::BUS8::BUS8_FCLK);
+
+        // Watchdog can see this. (Synchronous)
+        ::Multiplex::Multiplex::create_multiplex(Programs::WAKE_MULTIPLEX, Programs::WAKE_GHOST);
+        
+        // Promote the CPUs (Branches break sequential/stripping pattern)
+        //  CPUs now have 50 percent chance of winning.
+        //      They now have 1 turn loss max penalty. 
+        //      Performance is 0.5 to 1
+        //  DMA now has 50 percent chance of losing.
+        //      They now have 3+ turn loss max penalty. 
+        //      Performance is <0.25 to 1
+        bus_ctrl_hw->priority = (1 << 4) | (1 << 0);
+        
+        // Do not connect the dots (LEDs), charge the low side before scanning (This will turn the LEDs off)
+        //  Do use Dot correction though, which is above this implementation layer
+        memset(null_table, 0, sizeof(null_table));
+        null_table[0] = COLUMNS - 1;
+
+        header = STEPS;                                                                             // This needs to be one less than (n + 1)
+        counter = 0;
+        bank = 0;
+
+        {   // We use a decent amount of stack here (The compiler should figure it out)
+            uint16_t instructions[32];
+            uint8_t length = Programs::get_pmp_program(instructions, 32);
+
+            static const struct pio_program pio_programs = {
+                .instructions = instructions,
+                .length = length,
+                .origin = 0,
+            };
+            uint offset = pio_add_program(pio0, &pio_programs);
+            pio_sm_set_consecutive_pindirs(pio0, 0, ::Matrix::BUS8::BUS8_DATA_BASE, ::Matrix::BUS8::BUS8_DATA_LEN, true);
+        
+            // Verify Serial Clock
+            constexpr float x = 125000000.0 / (SERIAL_CLOCK * 2.0);                                 // Someday this two will be a four.
+
+            // PMP / SM
+            pio0->sm[0].clkdiv = ((uint32_t) floor(x) << PIO_SM0_CLKDIV_INT_LSB) | ((uint32_t) round((x - floor(x)) * 255.0) << PIO_SM0_CLKDIV_FRAC_LSB);
+            pio0->sm[0].pinctrl = (1 << PIO_SM0_PINCTRL_SIDESET_COUNT_LSB) | (6 << PIO_SM0_PINCTRL_OUT_COUNT_LSB) | ((::Matrix::BUS8::BUS8_DATA_BASE + 6) << PIO_SM0_PINCTRL_SIDESET_BASE_LSB) | (::Matrix::BUS8::BUS8_DATA_BASE << PIO_SM0_PINCTRL_OUT_BASE_LSB);
+            pio0->sm[0].shiftctrl = (1 << PIO_SM0_SHIFTCTRL_AUTOPULL_LSB) | (6 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) | (1 << PIO_SM0_SHIFTCTRL_OUT_SHIFTDIR_LSB);
+            pio0->sm[0].execctrl = (1 << PIO_SM1_EXECCTRL_OUT_STICKY_LSB) | (12 << PIO_SM1_EXECCTRL_WRAP_TOP_LSB);
+            pio0->sm[0].instr = pio_encode_jmp(offset);
+            __dsb();
+            hw_set_bits(&pio0->ctrl, 1 << PIO_CTRL_SM_ENABLE_LSB);
+            pio_sm_claim(pio0, 0);
+        }
+
+        {   // We use a decent amount of stack here (The compiler should figure it out)
+            uint16_t instructions[32];
+            uint8_t length = Programs::get_ghost_program(instructions, 32);
+
+            static const struct pio_program pio_programs = {
+                .instructions = instructions,
+                .length = length,
+                .origin = 0,
+            };
+            uint offset = pio_add_program(pio0, &pio_programs);
+
+            // Ghosting program
+            pio0->sm[1].clkdiv = (1 << PIO_SM0_CLKDIV_INT_LSB);
+            pio0->sm[1].pinctrl = (1 << PIO_SM0_PINCTRL_SIDESET_COUNT_LSB) | (::Matrix::BUS8::BUS8_OE << PIO_SM0_PINCTRL_SIDESET_BASE_LSB);
+            pio0->sm[1].shiftctrl = (1 << PIO_SM0_SHIFTCTRL_OUT_SHIFTDIR_LSB);
+            pio0->sm[1].execctrl = (31 << PIO_SM1_EXECCTRL_WRAP_TOP_LSB);
+            pio0->sm[1].instr = pio_encode_jmp(offset);
+            __dsb();
+            hw_set_bits(&pio0->ctrl, 2 << PIO_CTRL_SM_ENABLE_LSB);
+            pio_sm_claim(pio0, 1);
+        }
+        
+        // DMA
+        dma_chan[0] = dma_claim_unused_channel(true);
+        dma_chan[1] = dma_claim_unused_channel(true);
+        dma_chan[2] = dma_claim_unused_channel(true);
+        dma_chan[3] = dma_claim_unused_channel(true);
+        dma_channel_config c = dma_channel_get_default_config(dma_chan[0]);
+        channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
+        channel_config_set_read_increment(&c, true);
+        channel_config_set_high_priority(&c, true);
+        channel_config_set_dreq(&c, DREQ_PIO0_TX0);
+        channel_config_set_chain_to(&c, dma_chan[1]);
+        channel_config_set_irq_quiet(&c, true);
+        dma_channel_configure(dma_chan[0], &c, &pio0_hw->txf[0], NULL, 0, false);
+        dma_channel_set_irq0_enabled(dma_chan[0], true); 
+        
+        c = dma_channel_get_default_config(dma_chan[1]);
+        channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
+        channel_config_set_read_increment(&c, true);
+        channel_config_set_write_increment(&c, true);
+        channel_config_set_high_priority(&c, true);
+        channel_config_set_ring(&c, true, 3);                                           // 1 << 3 byte boundary on write ptr
+        dma_channel_configure(dma_chan[1], &c, &dma_hw->ch[dma_chan[0]].al3_transfer_count, &address_table[bank][0], 2, false);
+        
+        c = dma_channel_get_default_config(dma_chan[2]);
+        channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+        channel_config_set_read_increment(&c, true);
+        channel_config_set_write_increment(&c, false);
+        channel_config_set_high_priority(&c, false);   
+        channel_config_set_dreq(&c, DREQ_PIO0_TX1); 
+        channel_config_set_chain_to(&c, dma_chan[3]);
+
+        c = dma_channel_get_default_config(dma_chan[3]);
+        channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+        channel_config_set_read_increment(&c, true);
+        channel_config_set_write_increment(&c, false);
+        channel_config_set_high_priority(&c, false);   
+        channel_config_set_dreq(&c, DREQ_PIO0_TX1); 
+        channel_config_set_chain_to(&c, dma_chan[2]);
+        dma_channel_configure(dma_chan[3], &c, &pio0_hw->txf[1], &ghost_packet, 2, false);
+    }
+
+    void Multiplex::show(Payload *packet) {
+        if (packet != nullptr) {
+            while (!multicore_fifo_wready()) {                                                      // Watchdog can see this. (Synchronous)
+                // Do nothing
+            }
+
+            multicore_fifo_push_blocking(reinterpret_cast<uint32_t>(packet));                       // Translate
+
+            while (multicore_fifo_rvalid()) {                                                       // Watchdog can see this. (Synchronous)
+                 delete reinterpret_cast<Payload *>(multicore_fifo_pop_blocking());                  // Translate
+            }           
+        }
+    }
+
+    void __not_in_flash_func(Multiplex::send_buffer)() {
+        __dsb();
+        dma_hw->ints0 = 1 << dma_chan[0];
+        dma_channel_set_read_addr(dma_chan[1], address_table[bank], true);
+        dma_channel_set_read_addr(dma_chan[3], &ghost_packet, true);
+    }
+
+    void Multiplex::load_buffer(Payload *packet) {
+        // TODO: Rework data structures
+    }
+
+    // Warning: we are high priority here.
+    //  We need to be called two times the refresh rate at least.
+    //  If we get called to often we stall.
+    void __not_in_flash_func(Multiplex::work)() {
+        bool swapable = false;
+
+        while (1) {                                                                                 // Wait for first packet
+            if (multicore_fifo_rvalid()) {
+                load_buffer(reinterpret_cast<Payload *>(multicore_fifo_pop_blocking()));             // Translate
+                send_buffer();
+                break;
+            }
+        }
+
+        while (1) {
+            if (dma_channel_get_irq0_status(dma_chan[0])) {
+                if (swapable && gpio_get(::Matrix::BUS8::BUS8_FCLK)) {
+                    while (!multicore_fifo_wready()) {                                              // Watchdog can see this. (Synchronous)
+                        // Do nothing
+                    }
+
+                    multicore_fifo_push_blocking(reinterpret_cast<uint32_t>(packets[bank]));        // Translate
+                    bank = (bank + 1) % num_buffers;
+                }
+
+                while (!gpio_get(::Matrix::BUS8::BUS8_RCLK)) {
+                    // Do nothing
+                }
+
+                send_buffer();                                                                      // Kick off hardware
+            }
+
+            if (multicore_fifo_rvalid()) {                                                          // Try to get ahead
+                if (bank != counter) {                                                              // Watchdog can see this. (Synchronous)
+                    load_buffer(reinterpret_cast<Payload *>(multicore_fifo_pop_blocking()));         // Translate
+                    swapable = true;
+                }
+            }
+        }
+    }
+}
