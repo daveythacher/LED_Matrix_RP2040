35077a563b0197e3ef876caaec23b9904c1e34f4
David Thacher <davidethacher@gmail.com>
2025-05-30 15:34:47 -0400

    updates

--- LED_Matrix/src/Interface/Node/UART/UART.cpp
+++ LED_Matrix/src/Interface/Node/UART/UART.cpp
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
+#include "hardware/resets.h"
 #include "Interface/Node/UART/UART.h"
 #include "Interface/Node/UART/hw_config.h"
 #include "GPIO/GPIO.h"
@@ -16,16 +17,27 @@ namespace Interface::Node::UART {
         // IO
         gpio_init(0);
         gpio_init(1);
+        gpio_init(2);
+        gpio_init(3);
         gpio_set_dir(0, GPIO_OUT);
+        gpio_set_dir(3, GPIO_OUT);
         gpio_set_function(0, GPIO_FUNC_UART);
         gpio_set_function(1, GPIO_FUNC_UART);
+        gpio_set_function(2, GPIO_FUNC_UART);
+        gpio_set_function(3, GPIO_FUNC_UART);
         IO::GPIO::claim(0);
         IO::GPIO::claim(1);
+        IO::GPIO::claim(2);
+        IO::GPIO::claim(3);
 
         // UART
-        uart_init(uart0, SERIAL_UART_BAUD);
-
-        // TODO: Enable Hardware Flow control
+        reset_block(uart_get_index(uart0) ? RESETS_RESET_UART1_BITS : RESETS_RESET_UART0_BITS);
+        unreset_block_wait(uart_get_index(uart0) ? RESETS_RESET_UART1_BITS : RESETS_RESET_UART0_BITS);
+        uart_set_baudrate(uart0, SERIAL_UART_BAUD);
+        uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
+        hw_set_bits(&uart_get_hw(uart0)->lcr_h, UART_UARTLCR_H_FEN_BITS);
+        uart_get_hw(uart0)->dmacr = UART_UARTDMACR_TXDMAE_BITS | UART_UARTDMACR_RXDMAE_BITS;
+        uart_get_hw(uart0)->cr = UART_UARTCR_UARTEN_BITS | UART_UARTCR_TXE_BITS | UART_UARTCR_RXE_BITS | UART_UARTCR_RTSEN_BITS | UART_UARTCR_CTSEN_BITS;
     }
 
     UART *UART::create_node() {
